140de74000551e5e188df39544373c8939e83dfe
Bryan Cervantes <bryanandkyle@gmail.com>
2020-09-21 16:13:54 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -69,7 +69,13 @@ def turn_count
     end
 end
 
- 
+ def won?
+   WIN_COMBINATIONS.detect do |combo|
+      @board[combo[0]] == @board[combo[1]] &&
+      @board[combo[2]] == @board[combo[2]] &&
+      position_taken?(combo[0])
+    end 
+  end
   
  
   def valid_move?(index)
